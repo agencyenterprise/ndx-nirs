60cb111cc7d6357a26070d84f7cfaac6657901fd
sumner15 <sumner.norman@gmail.com>
2021-04-20 00:37:43 +0000

    slowly pruning errors in `make html`

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -52,7 +52,7 @@ language = "English"
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+exclude_patterns = ["*.ipynb_checkpoints*"]
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -65,7 +65,8 @@ html_theme = "alabaster"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
+#html_static_path = ["_static"]
+html_static_path = []
 
 
 # -- Extension configuration -------------------------------------------------
